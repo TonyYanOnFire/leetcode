e79d940af2e304fda301128cf3cb38d2b8065ebd
HongyuYan95 <55646026+HongyuYan95@users.noreply.github.com>
2022-04-21 00:00:19 +0800

    correct 847 to 874, add 874 comment

--- LC00874 walking-robot-simulation/solution.py
+++ LC00874 walking-robot-simulation/solution.py
@@ -1,4 +1,5 @@
-# lc874
+# https://leetcode-cn.com/problems/walking-robot-simulation/
+
 class Solution:
   def robotSim(self, commands: List[int], obstacles: List[List[int]]) -> int:
       max_dist = 0
@@ -33,3 +34,19 @@ class Solution:
               max_dist = max(max_dist, x ** 2 + y ** 2)
 
       return max_dist
+
+"""
+并不算难的一道题. 之前是简单难度, 可能是通过率比较低, 改成了困难.
+几个关键点: 使用哈希表而不是矩阵储存障碍物. 按照题设给出最大范围和最大障碍物数, 密度为1 / (9e4), 即空间利用率很低且开销大
+关键在于键值的计算方式. 此处利用了python的set hashable的特性. 如果语言没有这个特性可以考虑用字符串"x,y", 或者计算坐标序号 x * M + y, M为x轴范围
+
+针对类似的地图走格子问题, 还有一些技巧.
+处理方向时, 语言如果不支持负索引, 可以加上3, 即 - 1 + 4
+direction_index = (direction_index - 1 + 4) % 4
+
+方向的计算通过构建dx_list, dy_list计算
+directions = ['y', '-x', '-y', 'x']
+dx_list = [0, -1, 0, 1]
+dy_list = [1, 0, -1, 0]
+x, y = x + dx_list[direction_index], y + dy_list[direction_index]
+"""
