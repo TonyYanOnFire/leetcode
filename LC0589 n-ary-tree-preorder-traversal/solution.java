a598f94e17d94d0acbc3da3909c626ea2e596f12
HongyuYan95 <55646026+HongyuYan95@users.noreply.github.com>
2022-04-25 22:39:26 +0800

    add LC589 n-ary-tree-preorder-traversal

---
+++ LC0589 n-ary-tree-preorder-traversal/solution.java
@@ -0,0 +1,36 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    public List<Integer> preorder(Node root) {
+        seq = new ArrayList<Integer>();
+        dfs(root);
+        return seq;
+    }
+
+    void dfs(Node root) {
+        if (root == null) return;
+        seq.add(root.val);
+        for (Node child : root.children) {
+            dfs(child);
+        }
+    };
+
+    List<Integer> seq;
+}
